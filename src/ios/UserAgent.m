9bbc5e54b09fd6603b09325da1f660e426f7b8fe
Yuki Honda <self@yukihonda.com>
2019-09-09 00:56:44 +0000

    change

--- src/ios/UserAgent.m
+++ src/ios/UserAgent.m
@@ -21,26 +21,26 @@
     // WKWebView* dummyWebView = [WKWebView new];
     // dummyWebView = [WKWebView new]; // デフォルトのUserAgentを取得するための別インスタンスを作る
     // [dummyWebView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id result, NSError* error) {
-        id newUserAgent = [command argumentAtIndex:0];
-        self.webView.customUserAgent = newUserAgent;
+        // id newUserAgent = [command argumentAtIndex:0];
+        // self.webView.customUserAgent = newUserAgent;
     // }];
     
     // id replaceUserAgent = [command argumentAtIndex:0];
-    NSString* callbackId = command.callbackId;
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-    // [CDVUserAgentUtil acquireLock:^(NSInteger lockToken) {
-    //     NSString* newUserAgent = [command argumentAtIndex:0];
-    //     AppDelegate* appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    //     appDelegate.viewController.baseUserAgent = newUserAgent;
-    //     self.webView.customUserAgent = (UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent") ?? "") + "/" + newUserAgent;
-    //     [CDVUserAgentUtil setUserAgent:newUserAgent lockToken:lockToken];
-    //     [CDVUserAgentUtil releaseLock:&lockToken];
+    // NSString* callbackId = command.callbackId;
+    // CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
+    // [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    [CDVUserAgentUtil acquireLock:^(NSInteger lockToken) {
+        NSString* newUserAgent = [command argumentAtIndex:0];
+        AppDelegate* appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        appDelegate.viewController.baseUserAgent = newUserAgent;
+        self.webView.customUserAgent = (UIWebView().stringByEvaluatingJavaScript(from: "navigator.userAgent") ?? "") + "/" + newUserAgent;
+        [CDVUserAgentUtil setUserAgent:newUserAgent lockToken:lockToken];
+        [CDVUserAgentUtil releaseLock:&lockToken];
         
-    //     NSString* callbackId = command.callbackId;
-    //     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
-    //     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-    // }];
+        NSString* callbackId = command.callbackId;
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }];
     
     // [self.webView evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id result, NSError* error) {
     //     id newUserAgent = [command argumentAtIndex:0];
