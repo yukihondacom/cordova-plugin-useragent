436d256c652f6c22b4f0dfd021d8fb03bc67aec2
Yuki Honda <self@yukihonda.com>
2019-09-13 00:14:45 +0000

    change

--- src/ios/UserAgent.m
+++ src/ios/UserAgent.m
@@ -20,8 +20,7 @@
 {
     [CDVUserAgentUtil acquireLock:^(NSInteger lockToken) {
         NSString* newUserAgent = [command argumentAtIndex:0];
-//        self.webView.customUserAgent = newUserAgent;
-        super.webView.customUserAgent = newUserAgent;
+        self.webView.customUserAgent = newUserAgent;
         [CDVUserAgentUtil setUserAgent:newUserAgent lockToken:lockToken];
         [CDVUserAgentUtil releaseLock:&lockToken];
         
